2a9aab651669707ff80ac70fce0892d49c19efd2
Matt Jolly <Matt.Jolly@footclan.ninja>
2023-11-14 16:16:37 +1000

    Tidy up configure for modern systems
    - Ran `autoupdate`
    - Dropped obsolete `STDC_HEADERS`
    - Use `HAVE_SYS_TIME_H` only; drop obsolete `TIME_WITH_SYS_TIME`

--- src/cronoutils.h
+++ src/cronoutils.h
@@ -94,16 +94,11 @@
 #include <sys/stat.h>
 
 
-#if TIME_WITH_SYS_TIME
-# include <sys/time.h>
-# include <time.h>
-#else
-# if HAVE_SYS_TIME_H
-#  include <sys/time.h>
-# else
-#  include <time.h>
-# endif
+#if HAVE_SYS_TIME_H
+#include <sys/time.h>
+#include <time.h>
 #endif
+
 
 
 #ifdef _WIN32
